52f0342f4454e5509e93aa8807bd3f60c3eb81ba
Andrew Kaynã <techkayna@gmail.com>
2025-05-12 20:50:54 -0300

    add fake dashboard screen

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__)
-app.secret_key = 'chave-secreta'  # Necessária para exibir mensagens flash
+app.secret_key = 'chave-secreta'  # Necessária para exibir mensagens flash e usar sessão
 
 # Simulação de "banco de dados" em memória
 usuarios = {}
@@ -16,8 +16,9 @@ def login():
     password = request.form.get('password')
 
     if username in usuarios and usuarios[username] == password:
+        session['usuario'] = username
         flash('Login realizado com sucesso!')
-        return redirect(url_for('index'))
+        return redirect(url_for('dashboard'))
     else:
         flash('Usuário ou senha inválidos.')
         return redirect(url_for('index'))
@@ -37,6 +38,20 @@ def cadastrar():
 
     usuarios[username] = password
     flash('Cadastro realizado com sucesso!')
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+def dashboard():
+    if 'usuario' in session:
+        return render_template('dashboard.html', usuario=session['usuario'])
+    else:
+        flash('Você precisa fazer login primeiro.')
+        return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    flash('Logout realizado com sucesso.')
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
